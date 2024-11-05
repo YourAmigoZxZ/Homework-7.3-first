9be2a74ec5e0b954aadfad5fc2e2d07d4dfc0ad0
YourAmigo <skoryhmaksim10@gmail.com>
2024-11-05 06:24:14 +0300

    Созданы все перегрузки, метод принт, реализована проверка работы в мейне

--- Overcoat.h
+++ Overcoat.h
@@ -9,19 +9,21 @@ private:
 	char* type;
 	int price;
 public:
+	//конструкторы
 	Overcoat() : Overcoat(nullptr, 0) {}
-	Overcoat(char* type_p, int price_p) : type{ type_p ? new char[strlen(type_p) + 1] : nullptr }, price{ price_p }
+	explicit Overcoat(int price_p) : Overcoat(nullptr, price_p) {}//конструктор преобразования типов
+	Overcoat(const char* type_p, int price_p) : type{ type_p ? new char[strlen(type_p) + 1] : nullptr }, price{ price_p }
 	{
 		if (type)
 		{
 			strcpy_s(type, strlen(type_p) + 1, type_p);
 		}
 	}
-	Overcoat(const Overcoat& coat) : type { new char [strlen(coat.type)] }, price { coat.price }
+	Overcoat(const Overcoat& coat) : type { new char [strlen(coat.type)] }, price { coat.price }//конструктор копирования
 	{
 		strcpy_s(type, strlen(coat.type) + 1, coat.type);
 	}
-
+	//аксессоры
 	const char* get_type() const
 	{
 		return type;
@@ -30,7 +32,7 @@ public:
 	{
 		return price;
 	}
-
+	//модификаторы
 	void set_type(char* type_p)
 	{
 		if (type)
@@ -45,6 +47,48 @@ public:
 	void set_price(int price_p)
 	{
 		price = price_p;
+	}
+	
+	void print() const//метод вывода
+	{
+		printf("%s - %d\n\n", type, price);
+	}
+	//перегрузки операторов
+	friend bool operator==(const Overcoat& coat1, const Overcoat& coat2)
+	{
+		return coat1.type == coat2.type;
+	}
+
+	Overcoat& operator=(const Overcoat& coat)
+	{
+		if (this == &coat)//проверка на самоприравнивание
+		{
+			return *this;
+		}
+
+		if (type != nullptr)
+		{
+			delete[] type;
+		}
+
+		type = new char[strlen(coat.type) + 1];
+		strcpy_s(type, strlen(coat.type) + 1, coat.type);
+		price = coat.price;
+
+		return *this;
+	}
+
+	friend bool operator>(const Overcoat& coat1, const Overcoat& coat2)
+	{
+		if (strcmp(coat1.type, coat2.type) == 0)//проверка на одинаковые типы
+		{
+			return coat1.price > coat2.price;
+		}
+		else
+		{
+			cout << "Тип верхней различается";
+			return false;
+		}
 	}
 };
 
--- Source.cpp
+++ Source.cpp
@@ -4,7 +4,33 @@ int main()
 {
 	setlocale(LC_ALL, "rus");
 
+	Overcoat cloth1{ "coat", 11400 };
+	Overcoat cloth2{ "raincoat", 2400 };
+	Overcoat cloth3{ "coat", 12700 };
+	int user_choice;
 
+	cout << "В наличии 3 объекта верхней одежды:\n";
+	cloth1.print();
+	cloth2.print();
+	cloth3.print();
+
+	if (cloth1 == cloth2)
+	{
+		cout << "\n\nОдежда номер 1 и одежда номер 2 одинакового типа\n\n";//мы не попадём сюда, т.к. это не так
+	}
+	else if (cloth1 == cloth3)
+	{
+		cout << "\n\nОдежда номер 1 и одежда номер 3 одинакового типа\n\n";//Утверждение верно
+	}
+
+	cloth2 = cloth3;
+	cout << "Вывод изменённого второго объекта(плащ): ";
+	cloth2.print();//выводится обьект 2, который уже равен объекту 3
+
+	if (cloth3 > cloth1)
+	{
+		cout << "Цена третьего объекта выше, чем цена первого";
+	}
 
 	return 0;
 }
